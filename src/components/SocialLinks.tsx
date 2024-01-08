1acc123482b8affa6ad57f4cc53f98b8ec3b0ac3
bobirrahmatov <55207867+bobirrahmatov@users.noreply.github.com>
2024-01-08 18:51:00 +0100

    version 3

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -4,18 +4,17 @@ import constants from '../../const/config';
 
 // Social links (remove any you don't want to show by simply deleting the line)
 const socialLinksArray: { link: string; svgPath: string; }[] = [];
-const { twitter, instagram, discord, telegram, email, website } = constants.socialLinks;
-if (twitter) socialLinksArray.push({link: twitter, svgPath: "/images/twitter.svg"});
-if (instagram) socialLinksArray.push({link: instagram, svgPath: "/images/instagram.svg"});
-if (discord) socialLinksArray.push({link: discord, svgPath: "/images/discord.svg"});
-if (telegram) socialLinksArray.push({link: telegram, svgPath: "/images/telegram.svg"});
-if (website) socialLinksArray.push({link: website, svgPath: "/images/website.svg"});
-if (email) socialLinksArray.push({link: email, svgPath: "/images/email.svg"});
+const { twitter, instagram, telegram, email, website } = constants.socialLinks;
+if (twitter) socialLinksArray.push({ link: twitter, svgPath: "/images/twitter.svg" });
+if (instagram) socialLinksArray.push({ link: instagram, svgPath: "/images/instagram.svg" });
+if (telegram) socialLinksArray.push({ link: telegram, svgPath: "/images/telegram.svg" });
+if (website) socialLinksArray.push({ link: website, svgPath: "/images/website.svg" });
+if (email) socialLinksArray.push({ link: email, svgPath: "/images/email.svg" });
 
 const SocialLinks: React.FC = () => {
   return (
     <div className="flex justify-center space-x-2 sm:space-x-4 mt-6">
-      {socialLinksArray.map(({link, svgPath}, i) => (
+      {socialLinksArray.map(({ link, svgPath }, i) => (
         <a
           href={link}
           target="_blank"
